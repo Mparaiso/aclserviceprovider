22edc223e10c584de4736870bdb8725918289d32
mparaiso <aikah@free.fr>
2012-08-05 21:04:57 +0200

    added post/put/delete handlers & form & makeroute

--- public/index.php
+++ public/index.php
@@ -10,6 +10,11 @@ get("/signup",function($app){
 	$app->render("signup");
 });
 
+post("/signup",function($app){
+	$app->set("message","Thanks for signing up ".
+	$app->form("name")."!");
+	$app->render("home");
+});
 get("/start",function($app){
 	return "Start";
 });
--- views/signup.php
+++ views/signup.php
@@ -1,1 +1,6 @@
 <h2>Sign up now !</h2>
+<form action="<?=$this->make_route('/signup')?>" method="post">
+	<label for="name">Name</label>
+	<input type="text" name="name" id="name">
+	<input type="submit" value="submit">
+</form>
